270c806b005441052a9da04004bf5c740013b40e
nullexception <nullexception@yandex.ru>
2013-12-30 09:40:53 +0400

    tree with lights

--- run_me.py
+++ run_me.py
@@ -8,12 +8,12 @@ import sys
 from power_console import *
 import random
 
-date = """
+date = u"""
 ★░★░★░★░▀▀▀▀█░░█▀▀▀█░░▀█░░█░░░█░★░★░★░★ 
 ★░★░★░★░█▀▀▀▀░░█░░░█░░░█░░▀▀▀▀█░★░★░★░★ 
 ★░★░★░★░▀▀▀▀▀░░▀▀▀▀▀░░░▀░░░░░░▀░★░★░★░★"""
 
-lable = """
+lable = u"""
 ✷░░░█▀▀░░█░█░█▀█░█▀█░░█░░░█░█▀▌░▐▀█░░░✷
 ❂░░░█░░░░█▀█░█░█░█▀▀█░█▀█░█░█░▀█▀░█░░░❂
 ✹░░░▀▀▀░░▀░▀░▀▀▀░▀▀▀▀░▀▀▀░▀░▀░░▀░░▀░░░✹
@@ -22,7 +22,7 @@ lable = """
 ✸░░░░░░▀░░░▀▀▀░▐▀▀▀▌░▀▀▀░▀░░▀░░▀░░░░░░✸
 """
 
-text = """
+text = u"""
 Дорогие друзья! Поздравляю вас с Новым 2014 годом!
 Желаю всем вам в наступающем году много новых и интересных задач!
 Чтобы количество начатых проектов равнялось количеству успешных релизов!
@@ -30,26 +30,6 @@ text = """
 Дорогие друзья, желаю, чаще покидать околокопьютерное-диванное пространство и 
 проводить время вместе с друзьми, любимыми и близкими вам людьми 
 (поверьте им вас не хватает).
-"""
-
-tree_0 = """  ★                                  
-              8                                      
-             G8I                                      
-            OZ7$D                                  
-           56+~I?8                                  
-          ZZ$ZZZ$OZ                                  
-         $?=?I=II+?8                                  
-        8OZZZZZZZZZ8O                               
-       9=ZI=??=?I+I$+D                               
-      OZ$8O$ZZ$ZZZO8OZ8                               
-     0Z?=ZI+I?+II?IZ?=;F                               
-    ZOOZ$OZ$$Z$Z$ZZOZ7OOZO                            
-   $I+$++Z?+II=???I$?=$?+DR                            
-  DO7IZ77O$?7$I7$77O7IZ7I8TY                            
- II+Z?+Z?+ZI=?I=I?+IZ?=$+=$?I                         
-D$$7Z7IZ77Z7I7$I777$Z7IZ7IZ7I7                         
-            DI+?                                      
-            D$77                                     
 """
 
 star = """
@@ -74,6 +54,27 @@ tree_trunk = """           ██
            ██"""
 
 
+tree_4_windows = u"""     
+              ★                                  
+              8                                      
+             G8I                                      
+            OZ7$D                                  
+           56+~I?8                                  
+          ZZ$ZZZ$OZ                                  
+         $?=?I=II+?8                                  
+        8OZZZZZZZZZ8O                               
+       9=ZI=??=?I+I$+D                               
+      OZ$8O$ZZ$ZZZO8OZ8                               
+     0Z?=ZI+I?+II?IZ?=;F                               
+    ZOOZ$OZ$$Z$Z$ZZOZ7OOZO                            
+   $I+$++Z?+II=???I$?=$?+DR                            
+  DO7IZ77O$?7$I7$77O7IZ7I8TY                            
+ II+Z?+Z?+ZI=?I=I?+IZ?=$+=$?I                         
+D$$7Z7IZ77Z7I7$I777$Z7IZ7IZ7I7                         
+            DDDD                                      
+            DDDD                                     
+"""
+
 
 def set_color_balls_on_tree(tree="", symbol_4_ball_set="", ball_symbol = ""):
   from random import randint
@@ -85,10 +86,6 @@ def set_color_balls_on_tree(tree="", symbol_4_ball_set="", ball_symbol = ""):
   max_balls_count_on_tree = max_count_4_replace/3
   min_balls_count_on_tree = max_count_4_replace/5
   realy_balls_count_on_tree = randint(min_balls_count_on_tree, max_balls_count_on_tree)
-  # print "max_count_4_replace: ", max_count_4_replace
-  # print "max_balls_count_on_tree: ", max_balls_count_on_tree
-  # print "min_balls_count_on_tree: ", min_balls_count_on_tree
-  # print "realy_balls_count_on_tree: ", realy_balls_count_on_tree
   row_num = len(rows) - 1
   while row_num >= 0:
     l = len(rows[row_num].split(symbol_4_ball_set))-1
@@ -106,30 +103,37 @@ def set_color_balls_on_tree(tree="", symbol_4_ball_set="", ball_symbol = ""):
         rows[row_num] = u""
         for i in l:
           rows[row_num] += unicode(i)
-        # print rows[row_num]
         max_count_4_replace = max_count_4_replace - replace_count
     row_num -= 1
-  # for i in rows:
-    # print i
   return rows
 
 ball_symbol = u"✹"
 symbol_4_ball_set = u"▓"
-tree_body = set_color_balls_on_tree(tree_1, symbol_4_ball_set, ball_symbol)
-while True:
-  printout(lable)
-  printout(star, YELLOW)
-  print tree_head
-  for row in tree_body:
-    for s in row:
-      if unicode(s) == ball_symbol:
-        printout(unicode(s), random.randint(1, 7))
-      else:
-        printout(unicode(s), GREEN)
-    print 
-  printout(tree_trunk,GREEN)
-  print
-  printout(text, RED)
-  # printout(date)
-  time.sleep(1)
+tree_body = [set_color_balls_on_tree(tree_1, symbol_4_ball_set, ball_symbol) for i in xrange(0,2,1)]
+# print len(tree_body)
 
+
+
+def show_all(star="", tree_head="", tree_body="", lable="", text=""):
+  i = 0
+  while True:
+    printout(lable)
+    printout(star, YELLOW)
+    print tree_head
+    for row in tree_body[i]:
+      for s in row:
+        if unicode(s) == ball_symbol:
+          printout(unicode(s), random.randint(1, 7))
+        else:
+          printout(unicode(s), GREEN)
+      print 
+    printout(tree_trunk,GREEN)
+    print
+    printout(text, RED)
+    if i < len(tree_body)-1:
+      i += 1
+    else:
+      i = 0
+    time.sleep(1)
+
+show_all(star, tree_head, tree_body, lable, text)
